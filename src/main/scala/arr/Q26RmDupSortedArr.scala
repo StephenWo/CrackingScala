89fcff22825e6c2ce6cdb7a6d26bd72486bc2a42
stephen <millerdaihe@gmail.com>
2018-11-05 22:55:06 -0500

    first push

---
+++ src/main/scala/arr/Q26RmDupSortedArr.scala
@@ -0,0 +1,15 @@
+package arr
+
+object Q26RmDupSortedArr {
+  def main(args: Array[String]): Unit ={
+    val nums = Array(1,1,2)
+    println(removeDuplicates(nums))
+  }
+
+  def removeDuplicates(nums: Array[Int]): Int = {
+
+
+
+  }
+
+}
